e6929b1e732da9643acd9845b121f6dd6fdd8373
JessicaBorja <jessicalizethborja@gmail.com>
2022-04-25 11:07:39 +0200

    add gripper cam video side by side

--- webapp/helpers/data_utils.py
+++ webapp/helpers/data_utils.py
@@ -10,10 +10,12 @@ import numpy as np
 class DataManager:
     def __init__(self, data_root: str, n_frames: int, grip_pt_h: bool = False) -> None:
         super().__init__()
-        self.tmp_dir = "./webapp/static/tmp/"
-        os.makedirs(self.tmp_dir, exist_ok=True)
-        self.save_data_dir = "./webapp/static/"
-        self.data_path = data_root
+        self.media_dir = './webapp/static/'
+        for cam in ["gripper", "static"]:
+            make_dir = os.path.join(self.media_dir, "%s_cam" % cam)
+            os.makedirs(make_dir, exist_ok=True)
+        self.save_data_dir = './webapp/static/'
+        self.data_path = data_path
         self.n_frames = n_frames
         self.grip_pt_h = grip_pt_h
         self.n_seq = 150
@@ -47,18 +49,22 @@ class DataManager:
 
         start_idx = self.filename_to_idx(start)
         end_idx = self.filename_to_idx(end)
-        seq_imgs = []
+        gripper_imgs, static_imgs = [], []
         for i in range(start_idx, end_idx + 1):
             frame_i = self.idx_to_filename(i)
             filename = os.path.join(dir, frame_i)
-            img = self.extract_img(filename)
-            seq_imgs.append(img)
-        # self.make_video(seq_imgs, video_name="tmp")
-        self.make_video(seq_imgs, video_name=video_tag)
+            imgs = self.extract_imgs(filename)
+            gripper_imgs.append(imgs["gripper"])
+            static_imgs.append(imgs["static"])
+        
+        self.make_video(gripper_imgs, video_name=video_tag, cam="gripper")
+        self.make_video(static_imgs, video_name=video_tag, cam="static")
         return video_tag
+   
+    def make_video(self, seq_imgs, fps=80, video_name="v", cam="static"):
+        folder_path = os.path.join(self.media_dir, "%s_cam" % cam)
+        video_path = os.path.join(folder_path, video_name)
 
-    def make_video(self, seq_imgs, fps=80, video_name="v"):
-        video_path = os.path.join(self.tmp_dir, video_name)
         w, h = seq_imgs[0].shape[:2]
         fourcc = cv2.VideoWriter_fourcc("V", "P", "8", "0")
         video = cv2.VideoWriter(video_path, fourcc, fps, (w, h))  # 30 fps
@@ -68,13 +74,16 @@ class DataManager:
         cv2.destroyAllWindows()
         video.release()
 
-    def extract_img(self, filename):
+    def extract_imgs(self, filename):
         data = np.load(filename, allow_pickle=True)
-        img = data["rgb_static"]
-        img = cv2.resize(img, (300, 300))
+        imgs = {}
+        for cam in ["gripper", "static"]:
+            img = data["rgb_%s" % cam]
+            img = cv2.resize(img, (300, 300))
+            imgs[cam] = img
         # cv2.imshow("static", img[:, :, ::-1])  # W, H, C
         # cv2.waitKey(1)
-        return img
+        return imgs
 
     def filename_to_idx(self, filename):
         return int(filename.split("_")[-1][:-4])
@@ -82,14 +91,55 @@ class DataManager:
     def idx_to_filename(self, idx):
         return "frame_%06d.npz" % idx
 
-    def read_data(self, play_data_path):
-        """
-        play_data_path -> day -> time
-        _data:(list)
-            - {'indx': [start_filename, end_filename],
-               'dir': directory of previous files,
-               'n_frames': end_frame - start_frame}
-        """
+    def read_data_unprocessed(self, play_data_path):
+        '''
+            play_data_path -> day -> time
+            _data:(list)
+                - {'indx': [start_filename, end_filename],
+                   'dir': directory of previous files,
+                   'n_frames': end_frame - start_frame}
+        '''
+        # Get all posible initial_frames
+        initial_frames = []
+        date_folder = glob.glob("%s/*" %play_data_path, recursive=True)
+        for path in date_folder:
+            data_path = os.path.basename(path)
+
+            # Get files in subdirectory
+            time_folder = glob.glob("%s/*" % path, recursive=True)
+            for dir in time_folder:
+                files = glob.glob("%s/**/frame_*.npz" %dir, recursive=True)
+                files.sort()
+                indices = range(0, len(files) - self.n_frames, self.n_frames//2)
+                files = [files[i] for i in indices]
+                files = files[:-self.n_frames]
+                initial_frames.extend(files)
+        
+        # Select n_seq random sequences
+        n_seq = min(len(initial_frames), self.n_seq)
+        initial_frames = np.random.choice(initial_frames, size=n_seq, replace=False)
+        frames_info = {}
+        _data = []
+        for frame_dir in initial_frames:
+            head, start_filename = os.path.split(frame_dir)
+            frame_idx = self.filename_to_idx(start_filename)
+            end_frame_idx = frame_idx + self.n_frames
+            end_filename = self.idx_to_filename(end_frame_idx)
+            frames_info = {'indx': [start_filename, end_filename],
+                           'dir': head,
+                           'n_frames': self.n_frames}
+            _data.append(frames_info)
+        self.save_json(_data)
+        return _data
+
+    def read_data_preprocessed(self, play_data_path):
+        '''
+            play_data_path -> day -> time
+            _data:(list)
+                - {'indx': [start_filename, end_filename],
+                   'dir': directory of previous files,
+                   'n_frames': end_frame - start_frame}
+        '''
         # Get all posible initial_frames
         initial_frames = []
         date_folder = glob.glob("%s/*" % play_data_path, recursive=True)
