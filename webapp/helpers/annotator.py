1b2e09c633e9392b6207373c8b6c6ff14729f73b
JessicaBorja <jessicalizethborja@gmail.com>
2022-08-23 12:10:33 -0700

    ids

--- webapp/helpers/annotator.py
+++ webapp/helpers/annotator.py
@@ -18,39 +18,48 @@ def annotate():
         # Handle requests coming from annotate.html
         # To save to database
         if request.form["submit_button"] == "next" or request.form["submit_button"] == "no_task":
-            _curr_ids = db.session.query(LangAnn.id).all()
-            if len(_curr_ids) > 0:
-                seq_id = max(_curr_ids)[0] + 1
+            _ids_annotated = db.session.query(LangAnn.id).all()
+            _user_ann_seq = db.session.query(LangAnn).filter(LangAnn.user_id==current_user.id)
+            _user_ann_seq = _user_ann_seq.all()
+            if len(_user_ann_seq) > 0:
+                # Get last annotation
+                seq_id = max([ann.seq_id for ann in _user_ann_seq])[0] + 1
             else:
-                seq_id = 1
+                if len(_ids_annotated) > 0:
+                    seq_id = max(_ids_annotated)[0] + 1 
+                else:
+                    seq_id = 1
+            
+            # If sequence_id is not labeled
+            if(seq_id < LangAnn.query.count()):
+                # Save to database
+                if "color_x" in request.form:
+                    color_x = request.form["color_x"]
+                else:
+                    color_x = ""
 
-            if "color_x" in request.form:
-                color_x = request.form["color_x"]
-            else:
-                color_x = ""
+                if "color_y" in request.form:
+                    color_y = request.form["color_y"]
+                else:
+                    color_y = ""
 
-            if "color_y" in request.form:
-                color_y = request.form["color_y"]
-            else:
-                color_y = ""
-
-            new_langdata = LangAnn(
-                seq_id=seq_id,
-                user_id=current_user.id,
-                task=request.form["task"] if request.form["submit_button"] == "next" else "no_task",
-                annotation="",
-                color_x=color_x,
-                color_y=color_y,
-            )
-            db.session.add(new_langdata)
-            db.session.commit()
+                new_langdata = LangAnn(
+                    seq_id=seq_id,
+                    user_id=current_user.id,
+                    task=request.form["task"] if request.form["submit_button"] == "next" else "no_task",
+                    annotation="",
+                    color_x=color_x,
+                    color_y=color_y,
+                )
+                db.session.add(new_langdata)
+                db.session.commit()
             seq_id += 1
         elif request.form["submit_button"] == "end":
             return redirect(url_for("views.home"))
     else:
         # GET: Loading for first time
         try:
-            seq_id = max(db.session.query(LangAnn.id).all())[0] + 1
+            seq_id = max(db.session.query(LangAnn.seq_id).all())[0] + 1
             if seq_id - 1 >= Sequences.query.count():
                 return redirect(url_for("views.completed"))
         except Exception:
